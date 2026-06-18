899081729e5a9d9f8a133448bcaf5b48585f1aef
Sawastik Bhullar <sawastik7@gmail.com>
2026-06-18 23:31:53 +0530

    added a basic expiry key feature which auto expires the key after 10 seconds

--- src/commands/set.js
+++ src/commands/set.js
@@ -1,6 +1,14 @@
 import { store } from "../storage/store.js";
+import { delFunc } from "./del.js";
 export const setFunc=(args,socket)=>{
 
     store.set(args[0],args[1]);
+
+
+
+    setTimeout(()=>{
+delFunc(args);
+    },10000);
+
 
 }
